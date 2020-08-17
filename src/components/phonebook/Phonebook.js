c7c9208b1a667374f08031558f97a1a31c755cb4
Anastasia-Dmv <Vesta50000@gmail.com>
2020-08-17 17:51:12 +0300

    2 phonebook

--- src/components/phonebook/Phonebook.js
+++ src/components/phonebook/Phonebook.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import ContactList from './contactList/ContactList';
+import React, { Component } from "react";
+import ContactList from "./contactList/ContactList";
 
-import Filter from './filter/Filter';
-import ContactForm from './contactForm/ContactForm';
-import { v4 as uuidv4 } from 'uuid';
-import style from './Phonebook.module.css';
-import PropTypes from 'prop-types';
+import Filter from "./filter/Filter";
+import ContactForm from "./contactForm/ContactForm";
+import { v4 as uuidv4 } from "uuid";
+import style from "./Phonebook.module.css";
+import PropTypes from "prop-types";
 
 export default class Phonebook extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
 
   addContact = (name, number) => {
@@ -25,25 +25,25 @@ export default class Phonebook extends Component {
       id: uuidv4(),
     };
 
-    this.setState(prevState => {
+    this.setState((prevState) => {
       return {
         contacts: [...prevState.contacts, contact],
       };
     });
   };
-  changeFilter = filter => {
+  changeFilter = (filter) => {
     this.setState({ filter });
   };
 
   getFilteredContacts = () => {
     const { contacts, filter } = this.state;
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
-  handleDelete = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
+  handleDelete = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
     }));
   };
 
@@ -59,9 +59,8 @@ export default class Phonebook extends Component {
         <ContactForm addContact={this.addContact} contacts={filteredContacts} />
         <h2>Contacts</h2>
 
-        {filteredContacts.length > 1 && (
-          <Filter value={filter} onChangeFilter={this.changeFilter} />
-        )}
+        <Filter value={filter} onChangeFilter={this.changeFilter} />
+
         {filteredContacts.length > 0 && (
           <ContactList
             deleteContact={this.handleDelete}
@@ -80,7 +79,7 @@ Phonebook.propTypes = {
         name: PropTypes.string,
         id: PropTypes.string,
         number: PropTypes.string,
-      }),
+      })
     ).isRequired,
     filter: PropTypes.string,
   }),
--- src/components/phonebook/filter/Filter.js
+++ src/components/phonebook/filter/Filter.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import style from './filter.module.css';
+import React from "react";
+import style from "./filter.module.css";
 
-export default function Filter({value, onChangeFilter}) {
-    return (
-        <div>
-            <input  className={style.input} type="text" value={value} onChange={e=>onChangeFilter(e.target.value)}/>
-        </div>
-    )
+export default function Filter({ value, onChangeFilter }) {
+  return (
+    <div>
+      <h4>Find contacts by name </h4>
+      <input
+        className={style.input}
+        type="text"
+        value={value}
+        onChange={(e) => onChangeFilter(e.target.value)}
+      />
+    </div>
+  );
 }
